438f877cdc36134a6b2f4b602d60a8da2ab960a9
Jen Merritt <jenjomez@gmail.com>
2020-01-14 18:06:34 +0000

    convert euros to selected currency

--- src/app.js
+++ src/app.js
@@ -4,10 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-
+      currencies:null,
+      selectedCurrency: null,
+      euros: null
+    },
+    mounted(){
+      this.currencyFetch()
     },
     methods: {
-
+      currencyFetch: function(){
+        fetch('https://api.exchangeratesapi.io/latest')
+        .then(req => req.json())
+        .then(data => this.currencies = data.rates)
+        this.currencies = null;
+      },
+      currencyConversion: function(currency, euros){
+        const convertedAmount = currency * euros;
+        return convertedAmount
+      }
     }
   });
 });
